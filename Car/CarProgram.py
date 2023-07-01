52d31d0fc69d6d13ae75daa1e8597b4554f20992
Rei Kaizen <bondenie@gmail.com>
2023-07-01 19:36:18 +0800

    compile car prob to a folder

--- Car/CarProgram.py
+++ Car/CarProgram.py
@@ -33,17 +33,3 @@ class Car:
 # The get_speed method should return the current speed.
     def get_speed(self):
         return self.__speed 
-
-# Next, design a program that creates a Car object then calls the accelerate method five times. 
-# After each call to the accelerate method, get the current speed of the car and display it. 
-# Then call the brake method five times. 
-
-vroom = Car(1980, "Ford Capri 2.8i")
-
-for i in range(5):
-    vroom.accelerate()
-    print(f"The {vroom._Car__year_model} {vroom._Car__make} current speed: {vroom._Car__speed} mph")
-
-for i in range(5):
-    vroom.brake()
-    print(f"The {vroom._Car__year_model} {vroom._Car__make} current speed: {vroom._Car__speed} mph")
