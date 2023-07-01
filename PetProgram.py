68faf6b055d89f49eaa27e3bf226c0f2d6e06066
Rei Kaizen <bondenie@gmail.com>
2023-07-02 00:56:39 +0800

    delete needless comments

--- PetProgram.py
+++ PetProgram.py
@@ -43,19 +43,3 @@ class Pet:
 # This method returns the value of the _ _age field.   
     def get_age(self):
         return self.__age
-    
-
-# # Once you have written the class,
-# # write a program that creates an object of the class and prompts  the user -
-# # to enter the name, type, and age of his or her pet. 
-# # This data should be stored as the object’s attributes. 
-# # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
-
-# #test
-# name = input("Pet name: ")
-# type = input("Type: ")
-# age = input("Age: ")
-
-# pal = Pet(name, type, age)
-
-# print(f"Pet Identification: {pal.get_name()}, {pal.get_animal_type()}, {pal.get_age()}")
