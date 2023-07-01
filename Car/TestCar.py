0033cd49a693515a5e9eff6fb5095049f3e1fee4
Rei Kaizen <bondenie@gmail.com>
2023-07-01 20:41:55 +0800

    add a dash of lively, dynamic elements

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -7,6 +7,28 @@ import time
 
 def TestCar():
     vroom = Car(1980, "Ford Capri 2.8i")
+    
+    #red light
+    print("------------")
+    print("| \U0001F534 \U000026AB \U000026AB |  READY!")
+    print("------------")
+    time.sleep(2)
+    
+    #yellow light
+    print("------------")
+    print("| \U000026AB \U0001F7E1 \U000026AB |  SET!")
+    print("------------")
+    time.sleep(2)
+   
+    #green light
+    print("------------")
+    print("| \U000026AB \U000026AB \U0001F7E2 |  GO!")
+    print("------------")
+    time.sleep(2)
+        
+    print("Vrooom Vrooomm Vrooommm \U0001F695\U0001F4A8")
+    time.sleep(2)
+    
 
     for i in range(5):
         vroom.accelerate()
@@ -18,6 +40,6 @@ def TestCar():
         print(f"The {vroom._Car__year_model} {vroom._Car__make} current speed: {vroom.get_speed()} mph")
         time.sleep(1)
         
-    print(f"The {vroom._Car__year_model} {vroom._Car__make} engine is shut off.")
+    print(f"The {vroom._Car__year_model} {vroom._Car__make} engine is shut off. \U0001F68F\U0001F696")
     
 TestCar()
