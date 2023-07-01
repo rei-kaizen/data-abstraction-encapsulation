85b363b2a18d79d49ead1c499c17f6001faebcdb
Rei Kaizen <bondenie@gmail.com>
2023-07-01 19:34:06 +0800

    remove needless comments

--- fan/FanProgram.py
+++ fan/FanProgram.py
@@ -41,12 +41,3 @@ class Fan:
         return self.__color
     def set_color(self, color):
         self.__color = color
-
-# # For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. 
-# # Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
-
-# Eureka = Fan(1, True, 10, "yellow")
-# print("EUREKA - Speed:", Eureka.get_speed(), "| Power:",Eureka.get_on(), " | Radius:",Eureka.get_radius(), "| Color:",Eureka.get_color())
-
-# Cooler = Fan(2, False, 5)
-# print("COOLER - Speed:", Cooler.get_speed(), "| Power:",Cooler.get_on(), "| Radius:",Cooler.get_radius(), " | Color:",Cooler.get_color())
