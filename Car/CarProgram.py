e4ebb517e63e8d23815257940b562ec112374291
Rei Kaizen <bondenie@gmail.com>
2023-07-02 23:43:50 +0800

    modify the programs by adding methods for better organization

--- Car/CarProgram.py
+++ Car/CarProgram.py
@@ -1,3 +1,5 @@
+import time
+
 # Write a class named Car that has the following data attributes:
 
 # - _ _year_model (for the car’s year model)
@@ -33,3 +35,40 @@ class Car:
 # The get_speed method should return the current speed.
     def get_speed(self):
         return self.__speed 
+
+    def red_light(self):
+        print("-" * 12)
+        print("| \U0001F534 \U000026AB \U000026AB |  \033[31mREADY!\033[0m")
+        print("-" * 12)
+        time.sleep(2)
+    
+    def yellow_light(self):
+        print("-" * 12)
+        print("| \U000026AB \U0001F7E1 \U000026AB |  \033[93mSET!\033[0m")
+        print("-" * 12)
+        time.sleep(2)
+    
+    def green_light(self):
+        print("-" * 12)
+        print("| \U000026AB \U000026AB \U0001F7E2 |  \033[32mGO!\033[0m")
+        print("-" * 12)
+        time.sleep(2)
+    
+    def car_sfx(self):    
+        print()    
+        print("vrooom vrooomm .... " * 2 + " \U0001F695 \U0001F4A8")
+        time.sleep(2)
+    
+    def roadway(self):    
+        print("\033[90m" + ("-" * 17 + "|")*2 + "-" * 17 + "\033[0m")
+    
+    def car_stop(self):
+        print(f"The {self.__year_model} {self.__make}  engine is shut off. \U0001F68F\U0001F696")
+        
+    def up_speed(self):
+        print(f"The {self.__year_model} {self.__make} current speed: {self.get_speed()} mph \U000021D1")
+        time.sleep(1)
+    
+    def down_speed(self):
+        print(f"The {self.__year_model} {self.__make} current speed: {self.get_speed()} mph \U000021D3")
+        time.sleep(1)
--- Car/TestCar.py
+++ Car/TestCar.py
@@ -1,5 +1,4 @@
 from CarProgram import Car
-import time
 
 # Next, design a program that creates a Car object then calls the accelerate method five times. 
 # After each call to the accelerate method, get the current speed of the car and display it. 
@@ -8,44 +7,21 @@ import time
 def TestCar():
     vroom = Car(1980, "Ford Capri 2.8i")
     
-    #red light
-    print("-" * 12)
-    print("| \U0001F534 \U000026AB \U000026AB |  \033[31mREADY!\033[0m")
-    print("-" * 12)
-    time.sleep(2)
-    
-    #yellow light
-    print("-" * 12)
-    print("| \U000026AB \U0001F7E1 \U000026AB |  \033[93mSET!\033[0m")
-    print("-" * 12)
-    time.sleep(2)
-   
-    #green light
-    print("-" * 12)
-    print("| \U000026AB \U000026AB \U0001F7E2 |  \033[32mGO!\033[0m")
-    print("-" * 12)
-    time.sleep(2)
-    
-    print()    
-    print("vrooom vrooomm .... " * 2 + " \U0001F695 \U0001F4A8")
-    time.sleep(2)
-    
-    #road string
-    print("\033[90m" + ("-" * 17 + "|")*2 + "-" * 17 + "\033[0m")
+    vroom.red_light()
+    vroom.yellow_light()
+    vroom.green_light()
+    vroom.car_sfx()
+    vroom.roadway()
 
     for i in range(5):
         vroom.accelerate()
-        print(f"The {vroom._Car__year_model} {vroom._Car__make} current speed: {vroom.get_speed()} mph \U000021D1")
-        time.sleep(1)
+        vroom.up_speed()
 
     for i in range(5):
         vroom.brake()
-        print(f"The {vroom._Car__year_model} {vroom._Car__make} current speed: {vroom.get_speed()} mph \U000021D3")
-        time.sleep(1)
+        vroom.down_speed()
     
-    print(f"The {vroom._Car__year_model} {vroom._Car__make} engine is shut off. \U0001F68F\U0001F696")
-    
-    #road string
-    print("\033[90m" + ("-" * 17 + "|")*2 + "-" * 17 + "\033[0m")
+    vroom.car_stop()    
+    vroom.roadway()
     
 TestCar()
